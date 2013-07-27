7596d1566246a8804503011e2f29a48f42bf55d8
Steven Alexander <steven.william.alexander@googlemail.com>
2013-07-27 15:49:32 +0100

    added phpinfo

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo ln -s /etc/httpd/sites-available/default /etc/httpd/sites-enabled/default
 sudo echo 'hello' > /var/www/html/index.html
+sudo echo '<?php phpinfo(); ?>' > /var/www/html/phpinfo.php
 sudo service iptables stop
 sudo apachectl restart
